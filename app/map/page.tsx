74c533d51e51eef10b2f2051308f9e4b15b0faa3
Abdullah Emir Öner <abdullahemironer@gmail.com>
2025-10-17 13:36:22 +0300

    s

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,8 +1,15 @@
 import dynamic from "next/dynamic";
+import Link from "next/link";
 
 // Map component’ini SSR kapalı olarak import et
 const MapNoSSR = dynamic(() => import("./Map"), { ssr: false });
 
 export default function Page() {
-  return <MapNoSSR />;
+  return(<div className="relative">
+    <Link href="/camera" className="absolute top-4 left-4 z-20 bg-white p-2 rounded shadow">
+    Camera
+    </Link>
+    <MapNoSSR />
+  </div>
+    );
 }
